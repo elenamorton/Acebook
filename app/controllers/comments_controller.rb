9ef5becda21cfb7601944b6dfb8b7412dad499a6
elenamorton <elena.uleia@gmail.com>
2017-06-26 13:32:23 +0100

    Adding comments model and migration.

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,27 @@
+class CommentsController < ApplicationController
+  def new
+    @comment = Comment.new(post_id: params[:post_id])
+  end
+
+  def index
+    @comment = Comment.all
+  end
+
+  def create
+    @comment = Comment.new(commentParams)
+    if @comment.save
+      redirect_to comments_path(@comment)
+    else
+      render 'new'
+    end
+  end
+
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:comment, :post_id)
+  end
+
+
+end
