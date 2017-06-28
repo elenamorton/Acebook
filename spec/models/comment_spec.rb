3e8dc4ac88710105dd9c95196013468b68f91a8c
Sam Pritchard <sampritchard@msn.com>
2017-06-28 14:43:52 +0100

    unit test for comment and posts ID

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  it { is_expected.to be }
+  it 'comments have a post ID' do
+  post = Post.new
+  comment = Comment.create(comment: "Hey", post_id: post.id)
+  expect(comment.post_id).to eq(post.id)
+end
 end
