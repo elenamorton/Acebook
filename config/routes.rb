de5e2d918776a0134ba0a4918be7b5bb367dc25d
elenamorton <elena.uleia@gmail.com>
2017-06-28 16:19:43 +0100

    Adding root route; all users feature tests passing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # resources :passwords, controller: "clearance/passwords", only: [:create, :new]
   # resource :session, controller: "clearance/sessions", only: [:create]
+  root :to => 'posts#index'
+
   resources :users, controller: 'users', only: 'create'
   # resources :users, controller: "clearance/users", only: [:create] do
   #   resource :password,
